9a1828eec473dc5717238de0cf963566955333ef
Dmitry Makarov <makarov.da@phystech.edu>
2022-12-26 16:57:37 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace YaDiskUpload
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxAuth = new System.Windows.Forms.GroupBox();
+            this.groupBoxUpload = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // groupBoxAuth
+            // 
+            this.groupBoxAuth.AutoSize = true;
+            this.groupBoxAuth.Location = new System.Drawing.Point(12, 12);
+            this.groupBoxAuth.Name = "groupBoxAuth";
+            this.groupBoxAuth.Size = new System.Drawing.Size(370, 426);
+            this.groupBoxAuth.TabIndex = 0;
+            this.groupBoxAuth.TabStop = false;
+            this.groupBoxAuth.Text = "Авторизация";
+            // 
+            // groupBoxUpload
+            // 
+            this.groupBoxUpload.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxUpload.AutoSize = true;
+            this.groupBoxUpload.Location = new System.Drawing.Point(422, 12);
+            this.groupBoxUpload.Name = "groupBoxUpload";
+            this.groupBoxUpload.Size = new System.Drawing.Size(370, 426);
+            this.groupBoxUpload.TabIndex = 1;
+            this.groupBoxUpload.TabStop = false;
+            this.groupBoxUpload.Text = "Загрузка файлов";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBoxUpload);
+            this.Controls.Add(this.groupBoxAuth);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxAuth;
+        private System.Windows.Forms.GroupBox groupBoxUpload;
+    }
+}
+
